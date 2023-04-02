d713ad7bc3ea54e8a46340eb9a12072189bb49a6
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2023-04-02 20:04:12 +0200

    Gallery sektion för dator

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+const burger = document.getElementById('burger');
+const nav = document.getElementById('closebtn');
+
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
   }
   
   function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-  }
+  document.getElementById("sidenav").style.width = "0";
+}
+
+burger.addEventListener('click', openNav);
+nav.addEventListener('click', closeNav);
