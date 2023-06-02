1049aae11267c9f4d5f17b84e9a763eb169d19a6
Karl-Sebastian-Olsson <karl.olsson@elev.ga.ntig.se>
2023-06-02 22:53:56 +0200

    Validering/testning fix

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function openNav() {
     document.getElementById("sidenav").style.width = "250px";
   }
   
-  function closeNav() {
+function closeNav() {
   document.getElementById("sidenav").style.width = "0";
 }
 
